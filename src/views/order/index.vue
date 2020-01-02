f5d091310cacd9888b298ca4456206d160b6fe14
sv <zxw12002@gmail.com>
2020-01-02 22:22:23 +0800

    Ready:02

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -34,38 +34,32 @@
       <el-table
         :data="tableData"
         border
-        style="width: 100%"
+        class="w100"
       >
         <el-table-column
           fixed
           prop="date"
           label="日期"
-          width="150"
         />
         <el-table-column
           prop="name"
           label="姓名"
-          width="120"
         />
         <el-table-column
           prop="province"
           label="省份"
-          width="120"
         />
         <el-table-column
           prop="city"
           label="市区"
-          width="120"
         />
         <el-table-column
           prop="address"
           label="地址"
-          width="300"
         />
         <el-table-column
           prop="zip"
           label="邮编"
-          width="120"
         />
         <el-table-column
           fixed="right"
@@ -78,7 +72,7 @@
         </el-table-column>
       </el-table>
 
-      <el-card v-show="pageData.total>0" shadow="never" class="mt-1"><el-pagination background layout="prev, pager, next, jumper" :total="pageData.total" :current-page.sync="pageData.pn" @current-change="handleCurrentChange" /></el-card>
+      <el-card shadow="never" class="mt-1"><el-pagination background layout="prev, pager, next, jumper" :total="pageData.total" :current-page.sync="pageData.pn" @current-change="handleCurrentChange" /></el-card>
     </el-card>
   </div>
 </template>
